2cebd439ac8c52fb857dadd61fae590bea2fb0a6
Princelin7 <princelinshinyp.ug17.it@francisxavier.ac.in>
2025-09-29 10:06:25 +0530

    task2

--- problem2/app_status.sh
+++ problem2/app_status.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 APP_URL="http://localhost:4499/"  
 
+
 LOG_FILE="$HOME/app_status.log"
 
+
 NOW=$(date +"%Y-%m-%d %H:%M:%S")
+
 
 HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" $APP_URL)
 
